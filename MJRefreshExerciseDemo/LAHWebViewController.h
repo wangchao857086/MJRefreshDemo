712bcbcf97bd45a71ea2e39d06ddf97f6ca97a95
wangchao857086 <wangchao@bushangban.com>
2016-07-29 14:37:43 +0800

    first commit

--- MJRefreshExerciseDemo/LAHWebViewController.h
+++ MJRefreshExerciseDemo/LAHWebViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LAHWebViewController.h
 //  MJRefreshExerciseDemo
 //
 //  Created by 王超 on 16/7/29.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LAHWebViewController : UIViewController
 
 @end
-
---
+++ MJRefreshExerciseDemo/LAHWebViewController.m
@@ -0,0 +1,13 @@
+//
+//  LAHWebViewController.m
+//  MJRefreshExerciseDemo
+//
+//  Created by 王超 on 16/7/29.
+//  Copyright © 2016年 Walker. All rights reserved.
+//
+
+#import "LAHWebViewController.h"
+
+@implementation LAHWebViewController
+
+@end
